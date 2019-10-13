7483e1f6a698d9d42fef00b243973d3230653423
agbilotia1998 <iit2016508@iiita.ac.in>
2019-10-13 18:47:48 +0530

    Sets up initial connection between client and KDC AS

---
+++ KDC/KDC.py
@@ -0,0 +1,73 @@
+import hashlib, random, string, Padding, base64
+from flask import Flask, request, make_response, jsonify, json
+from flask_sqlalchemy import SQLAlchemy
+from Crypto.Cipher import AES
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Clients(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    password = db.Column(db.String(250))
+
+
+class TGS(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    password = db.Column(db.String(250))
+    is_available = db.Column(db.Integer)
+
+
+@app.route('/authenticating_server')
+def authenticating_server():
+    client_id = request.headers.get('id')
+    print('Request from Client ID ' + str(client_id))
+    client = Clients.query.filter_by(id=client_id).scalar()
+
+    if not client:
+        return make_response('Client not valid, dropping connection', 400)
+
+    tgs = TGS.query.filter_by(is_available=1).scalar()
+
+    if not tgs:
+        return make_response('Client not valid, dropping connection', 400)
+
+    client_secret_key = hashlib.md5(client.password.encode('utf-8'))
+    print(client_secret_key)
+    tgs_secret_key = hashlib.md5(tgs.password.encode('utf-8'))
+
+    sk1 = "".join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(16))
+    print("Session key " + sk1)
+    encryption_obj = AES.new(client_secret_key.hexdigest().encode(), AES.MODE_CBC, client_secret_key.hexdigest()[:16].encode())
+    # encrypted_sk1 = encryption_obj.encrypt(sk1)
+
+    life_span = 1000
+    tgt = "".join(str(client_id) + ' ' + str(request.remote_addr) + ' ' + str(life_span) + ' ' + str(datetime.now()) + sk1)
+    encryption_obj = AES.new(tgs_secret_key.hexdigest().encode(), AES.MODE_CBC, tgs_secret_key.hexdigest()[:16].encode())
+    padded_tgt = Padding.appendPadding(tgt, AES.block_size, mode='CMS')
+    encrypted_tgt = encryption_obj.encrypt(padded_tgt)
+
+    response = {
+        "sk1": sk1,
+        "tgt": base64.b64encode(encrypted_tgt).decode()
+    }
+    encryption_obj = AES.new(client_secret_key.hexdigest().encode(), AES.MODE_CBC, client_secret_key.hexdigest()[:16].encode())
+    padded_response = Padding.appendPadding(json.dumps(response), AES.block_size, mode='CMS')
+    encrypted_response = encryption_obj.encrypt(padded_response)
+
+    print(response)
+    return make_response({'data': base64.b64encode(encrypted_response).decode()}, 200)
+
+@app.route('/tgs')
+def tgs():
+    data = request.headers
+    authenticator = data.get('authenticator')
+    tgt = data.get('tgt')
+
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(port=8080, debug=True)
---
+++ client.py
@@ -0,0 +1,55 @@
+import hashlib
+import requests, base64
+from flask import json
+from Crypto.Cipher import AES
+from datetime import datetime
+
+
+class Client:
+    kdc_url = 'http://localhost:8080'
+    server_url = 'http://localhost:8081'
+
+    def __init__(self):
+        self.get_client_parameters()
+        self.compute_client_hash()
+
+    def get_client_parameters(self):
+        self.id = 1
+        self.password = 'client_pass'
+        self.address = '127.0.0.1'
+
+    def compute_client_hash(self):
+        self.secret_key = hashlib.md5(self.password.encode('utf-8'))
+        print('Client Secret Key ' + str(self.secret_key))
+
+    def request_as(self):
+        headers = {'id': str(self.id)}
+        response = requests.get(self.kdc_url + '/authenticating_server', headers=headers)
+        if response.status_code == 200:
+            data = json.loads(response.content)['data']
+            data = base64.b64decode(data)
+            decryption_obj = AES.new(self.secret_key.hexdigest().encode(), AES.MODE_CBC,
+                                     self.secret_key.hexdigest()[:16].encode())
+            decrypted_data = decryption_obj.decrypt(data).decode().strip()
+            sk1 = decrypted_data[9:25]
+            tgt = decrypted_data[36:-(4)]
+            print(decrypted_data)
+            print(sk1)
+            print(tgt)
+            authenticator = ''.join(str(self.id) + ' ' + str(self.address) + ' ' + str(datetime.now()) + ' ')
+            encryption_obj = AES.new(sk1, AES.MODE_CBC, sk1)
+            encrypted_authenticator = encryption_obj.encrypt(authenticator)
+
+            tgs_request_parameters = {
+                'authenticator': base64.b64encode(encrypted_authenticator).decode(),
+                'tgt': tgt
+            }
+            tgs_response = requests.get(url=self.kdc_url + '/tgs', headers=tgs_request_parameters)
+
+        else:
+            print(response.content)
+
+
+if __name__ == '__main__':
+    client = Client()
+    client.request_as()
